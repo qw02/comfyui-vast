661ec975317d05bf57f10f83f9dcbb918b42bdf5
qw02 <lowqingwen95@gmail.com>
2026-01-07 17:51:26 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ RUN --mount=type=cache,target=/var/cache/apt,sharing=locked \
     apt-get update && \
     apt-get install -y --no-install-recommends \
         python3.12 python3.12-venv python3.12-dev \
-        python3-pip \
+        python3-pip openssh-server \
         curl ffmpeg ninja-build git aria2 git-lfs wget vim \
         libgl1 libglib2.0-0 build-essential gcc && \
     \
--- src/start_script.sh
+++ src/start_script.sh
@@ -1,3 +1,13 @@
-git clone https://github.com/qw02/comfyui-vast.git
-mv comfyui-vast/src/start.sh /
-bash /start.sh
+#!/usr/bin/env bash
+set -e
+
+REPO_URL="https://github.com/qw02/comfyui-vast.git"
+REPO_DIR="/opt/comfyui-vast"
+
+if [ ! -d "$REPO_DIR/.git" ]; then
+    git clone "$REPO_URL" "$REPO_DIR"
+else
+    git -C "$REPO_DIR" pull
+fi
+
+exec bash "$REPO_DIR/src/start_script.sh"
